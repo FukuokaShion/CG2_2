ad75387bd3856865193cd80239ff07b113f254b6
Fukuoka_Shion <k021g1250@g.neec.ac.jp>
2022-06-30 14:41:10 +0900

    05_03完了

--- main.cpp
+++ main.cpp
@@ -278,11 +278,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	//単位行列を代入
 	constMapTransform->mat = XMMatrixIdentity();
 
-	constMapTransform->mat.r[0].m128_f32[0] = 2.0f / 1280.0f;
-	constMapTransform->mat.r[1].m128_f32[1] = -2.0f / 720.0f;
-	constMapTransform->mat.r[3].m128_f32[0] = -1.0f;
-	constMapTransform->mat.r[3].m128_f32[1] = 1.0f;
+	//射影変換行列(透視投影)
+	XMMATRIX matProjection = XMMatrixPerspectiveFovLH(XMConvertToRadians(45.0f), (float)window_width / window_height, 0.1f, 1000.0f);
 
+	//定数バッファに転送
+	constMapTransform->mat = matProjection;
 
 #pragma endregion DirectX初期化処理
 	//DirectX初期化処理　ここまで
@@ -299,10 +299,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// 頂点データ
 	Vertex vertices[] = {
 		// x      y     z       u     v
-		{{0.0f, 100.0f, 0.0f}, {0.0f, 1.0f}}, // 左下
-		{{0.0f, 0.0f, 0.0f}, {0.0f, 0.0f}}, // 左上
-		{{100.0f, 100.0f, 0.0f}, {1.0f, 1.0f}}, // 右下
-		{{100.0f, 0.0f, 0.0f}, {1.0f, 0.0f}}, // 右上
+		{{-50.0f, -50.0f, 50.0f}, {0.0f, 1.0f}}, // 左下
+		{{-50.0f, 50.0f, 50.0f}, {0.0f, 0.0f}}, // 左上
+		{{50.0f, -50.0f, 50.0f}, {1.0f, 1.0f}}, // 右下
+		{{50.0f, 50.0f, 50.0f}, {1.0f, 0.0f}}, // 右上
 	};
 
 	// インデックスデータ
